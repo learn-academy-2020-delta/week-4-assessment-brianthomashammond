83edf61b62d05509cca410b2f8d21b990395d8b4
brianthomashammond <brianthomashammond@gmail.com>
2020-09-27 17:44:31 -0700

    Ready to pull

--- code-challenges.js
+++ code-challenges.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions
 
+console.log("\n", "Week 4 Assessment - Java Script Code Challenges - Brian Thomas Hammond");
+
 // --------------------1) Create a function that takes in an array. Each time the function is run, remove the first item from the array and shuffle the remaining content. If the array is empty, return "The array is empty."
 
 var collections = ["purple", "blue", "green", "yellow", "pink"]
@@ -7,8 +9,37 @@ var collections = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
+//Write a function that takes an array
+const shiftAndShuffle = (argumentArray) => {
+    //Remove the first element of the argumentArray using shift()
+    argumentArray.shift()
+    //Write a function to shuffle an array
+    const shuffle = (arr) => {
+        //Iterate over the length of the array from first element to last
+        for (let i = 0; i < arr.length; i++) {
+            //Generate a new random number on each iteration based on the array's length
+            const rand = Math.floor(Math.random() * (arr.length));
+            //Switch the value at the loop's current index with the value at the index determined by the random number
+            [arr[i], arr[rand]] = [arr[rand], arr[i]]
+        }
+        //Return the shuffled array
+        return arr
+    }
+    //Using the ternary operator, ask if argumentArray is empty
+    //If so, set argumentArray to the string "The array is empty."
+    //If not, shuffle() the argumentArray
+    argumentArray.length === 0 ? argumentArray = "The array is empty." : shuffle(argumentArray)
+    //Return argumentArray
+    return argumentArray
+}
 
 
+console.log("\n", "#1:");
+console.log(shiftAndShuffle(collections));// expect: these to all be different
+console.log(shiftAndShuffle(collections));// expect: these to all be different
+console.log(shiftAndShuffle(collections));// expect: these to all be different
+console.log(shiftAndShuffle(collections));// expect: these to all be different
+console.log(shiftAndShuffle(collections), "\n");// expect: these to all be different
 
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
@@ -19,7 +50,24 @@ var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
 
+//Write a function that takes an array of numbers
+const sumTheCubes = (array) => {
+    //Create a variable using the spread operator to hold a duplicate of the original array
+    var answerArray = [...array]
+    //Create a function to pass to reduce
+    const sum = (num1, num2) => {
+        return num1 + num2
+    }
+    //Map over the duplicate array, cubing each of them
+    //Reduce all numbers in the duplicate array to a single number using helper function above
+    //Return the reduced array
+    return answerArray.map( value => value ** 3).reduce(sum)
+}
 
+
+console.log("#2:");
+console.log(sumTheCubes(cubeAndSum1))// expect: 99
+console.log(sumTheCubes(cubeAndSum2), "\n")// expect: 1125
 
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
@@ -29,8 +77,18 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+//Write a function that takes an array of numbers
+const minAndMax = (array) => {
+    //Use Math.min to find the smallest number in the array using the spread operator
+    //Use Math.max to find the largest number in the array using the spread operator
+    //Return an array with the smallest and largest numbers in that order
+    return [Math.min(...array), Math.max(...array)]
+}
 
 
+console.log("#3:");
+console.log(minAndMax(nums1));// expect: [-8, 90]
+console.log(minAndMax(nums2), "\n");// expect: [-59, 109]
 
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
@@ -40,8 +98,20 @@ var testString1 = "albatross"
 var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
+// Write a function that takes a string
+const lowerThenUpper = (string) => {
+
+    //Split the string into an array
+    //Map over the array using a ternary operator making letters at even indices lowercase and letters at odd indices uppercase
+    //Join the letters back into a string
+    //Return the new string
+    return string.split("").map((letter, index) => index % 2 === 0 ? letter.toLowerCase() : letter.toUpperCase()).join("")
+}
 
 
+console.log("#4:");
+console.log(lowerThenUpper(testString1));// expect: "aLbAtRoSs"
+console.log(lowerThenUpper(testString2), "\n");// expect: "jAbBeRwOcKy"
 
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
@@ -49,3 +119,54 @@ var testString2 = "jabberwocky"
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+//Write a function that takes two arrays
+const noDupeNumbers = (array1, array2) => {
+    //Create an empty array to hold the answer
+    answerArray = []
+    //Create a function to loop through an array
+    const pushFirstInstance = (array) => {
+        //Iterate over the length of the array from first element to last
+        for (let i = 0; i < array.length; i++) {
+            //Push each element to the answer array if it is not already present
+            answerArray.includes(array[i]) ? "" : answerArray.push(array[i])
+        }
+    }
+    //Call pushFirstInstance() on both argument arrays
+    pushFirstInstance(array1)
+    pushFirstInstance(array2)
+    //Return the answer array
+    return answerArray
+}
+
+
+console.log("#5:");
+console.log(noDupeNumbers(arr1, arr2), "\n");// expect: [3, 7, 10, 5, 4, 8, 2, 1]
+
+
+var arr3 = [0, 4, 2, 43, 75, 8]
+
+
+//Write a function that takes an arbitrary number of arrays using the spread operator
+const noDupeNumbersStretch = (...arrays) => {
+    //Create an empty array to hold the answer
+    answerArray = []
+    //Combine all argument arrays into a single array
+    argumentsArray = [].concat(...arrays)
+    //Create a function to loop through an array
+    const pushFirstInstance = (array) => {
+        //Iterate over the length of the array from first element to last
+        for (let i = 0; i < array.length; i++) {
+            //Push each element to the answer array if it is not already present
+            answerArray.includes(array[i]) ? "" : answerArray.push(array[i])
+        }
+    }
+    //Call pushFirstInstance() on the collection of argument arrays
+    pushFirstInstance(argumentsArray)
+    //Return the answer array
+    return answerArray
+}
+
+
+console.log("#5 STRETCH:");
+console.log(noDupeNumbersStretch(arr1, arr2, arr3), "\n");// expect: [3, 7, 10, 5, 4, 8, 2, 1, 0, 43, 75]
--- code_challenges.rb
+++ code_challenges.rb
@@ -1,5 +1,9 @@
 # ASSESSMENT 4: Ruby Coding Practical Questions
 
+puts "
+Week 4 Assessment - Ruby Code Challenges - Brian Thomas Hammond
+"
+
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
 
 fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
@@ -7,42 +11,120 @@ fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
 
+#Write a method that takes an array
+def odds_small_to_big (array) 
+    #Use .select, .is_a? Numeric, and .is_odd? to pull odd numbers
+    #Use .sort to sort the numbers, return implicitly
+    p array.select {|item| item.is_a? Numeric and item.odd?}.sort
+end
 
 
-
+puts "
+#1:
+"
+odds_small_to_big(fullArr1)# Expected output: [-9, 7, 9, 199]
+odds_small_to_big(fullArr2)# Expected output: [-823, 7, 23]
 
 # --------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
 
+#Write a class named Bike
+class Bike
+    #Use attr_accessor to provide getter methods 
+    attr_accessor :model, :frame_size, :wheels, :speedometer
+    #Initialize to define model, frame size and wheels (default 2)
+    def initialize (model, frame_size, wheels = 2)
+        @model = model
+        @frame_size = frame_size
+        @wheels = wheels
+        @speedometer = 0
+    end
+    #Write method to print a decription of the bike's attributes
+    def get_info
+        p "The #{@model} bike has #{@wheels} wheels and a #{@frame_size} frame."
+    end
 
+    #Write method to hear bell ring (or in this case, beep).
+    def ring_bell
+        p "Beep beep!"
+    end
 
+    #Write method to display speed of bike
+    def speed
+        p "Your current speed is: #{@speedometer} km/h."
+    end
 
+    #Write method to increase speed of bike and display current speed
+    def pedal_faster accel
+        #Increment speed by the value of the argument
+        @speedometer += accel
+        #Display updated speed
+        speed
+    end
+
+    #Write method to decrease speed of bike and display current speed
+    def brake decel
+        #Use ternary operator to check if the argument is greater than the current speed
+        #If so, decrement speed by the value of the argument
+        #If not, set speed to zero
+        @speedometer >= decel ? @speedometer -= decel : @speedometer = 0
+        #Finally, display updated speed
+        speed
+    end
+    
+end
+
+puts "
+#2:
+"
+#Insantiate a new bike with attributes
+my_bike = Bike.new("Trek","168cm")
+#Call get_info method to receive description
+my_bike.get_info
 
 # Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
 
 
 # -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
 
-
-
-
+puts "
+#3:
+"
+#Call ring_bell method to hear the bell
+my_bike.ring_bell
 
 # Expected output example: my_bike.ring_bell => 'Beep beep!'
 
 
 # -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
 
+puts "
+#4:
+"
 
-
-
+#Call speed method to display speed
+my_bike.speed
 
 # Expected output example: my_bike.speed => 0
 
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
 
-
-
-
+puts "
+#5:
+"
+#Call pedal_faster method to display updated speed
+my_bike.pedal_faster 10 #expect "Your current speed is: 10 km/h."
+my_bike.pedal_faster 15 #expect "Your current speed is: 25 km/h."
+my_bike.pedal_faster 5 #expect "Your current speed is: 30 km/h."
+my_bike.pedal_faster 10 #expect "Your current speed is: 40 km/h."
+#Call brake method to display updated speed
+my_bike.brake 15 #expect "Your current speed is: 25 km/h."
+my_bike.brake 20 #expect "Your current speed is: 5 km/h."
+my_bike.brake 7 #expect "Your current speed is: 0 km/h."
+my_bike.brake 3 #expect "Your current speed is: 0 km/h."
 
 # Expected output example: my_bike.pedal_faster 10 => 10
 # Expected output example: my_bike.brake 15 => 0
+
+puts"
+"
